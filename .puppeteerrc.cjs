5c5298db91d0ba02e85dc42924a56027c4c00d84
Dubmfj <danielhoyysiempre212@gmail.com>
2026-09-24 09:12:53 -0500

    fix puppeteer and server port5

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,5 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Asegura que Chromium se descargue dentro de la carpeta del proyecto
   cacheDirectory: join(__dirname, '.puppeteer-cache'),
 };
