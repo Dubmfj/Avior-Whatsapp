b1cbc001cbc749d1768f8f572868294b3322a83c
Dubmfj <danielhoyysiempre212@gmail.com>
2026-09-24 09:04:30 -0500

    fix puppeteer and server port3

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,5 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Garantiza que la carpeta del navegador viva dentro del build de Render
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
